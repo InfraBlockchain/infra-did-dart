d8147990320d74ac4674fd0c4aab82b62c4b8de8
Cute_Wisp <sweatpotato13@gmail.com>
2023-04-12 13:55:50 +0900

    feat: 🎸 change package struct to flutter ffi package from rust

--- lib/infra_did_dart.dart
+++ lib/infra_did_dart.dart
@@ -1,28 +1,130 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library infra_did_dart;
+import 'dart:ffi';
+import 'dart:io';
+import 'package:ffi/ffi.dart';
+import 'package:bip39/bip39.dart' as bip39;
+import './bindings.dart';
 
-export 'src/infra_did_dart_base.dart';
-export 'src/key/key.dart';
-export 'src/util/util.dart';
-export 'src/resolver/resolver.dart';
-export 'src/verifiable/verifiable.dart';
+const DYNAMIC_LIBRARY_FILE_NAME_ANDROID = "libinfra_did_binding.so";
+const DYNAMIC_LIBRARY_FILE_NAME_MACOS = "libinfra_did_binding.dylib";
 
-export 'src/infrablockchain/client.dart';
-export 'src/infrablockchain/models/account.dart';
-export 'src/infrablockchain/models/node_info.dart';
-export 'src/infrablockchain/models/block.dart';
-export 'src/infrablockchain/models/action.dart';
-export 'src/infrablockchain/models/action_block.dart';
-export 'src/infrablockchain/models/transaction.dart';
-export 'src/infrablockchain/models/primary_wrapper.dart';
-export 'src/infrablockchain/models/block_header_state.dart';
-export 'src/infrablockchain/models/abi.dart';
+/// Wraps the native functions and converts specific data types in order to
+/// handle C strings.
 
-export 'src/infrablockchain/numeric.dart';
-export 'src/infrablockchain/serialize.dart';
-export 'src/infrablockchain/eosdart_base.dart';
-export 'src/infrablockchain/models/transaction.dart';
+class InfraDID {
+  static final InfraDIDBindings _bindings =
+      InfraDIDBindings(InfraDID._loadLibrary());
 
-// TODO: Export any libraries intended for clients of this package.
+  static DynamicLibrary _loadLibrary() {
+    return Platform.isAndroid
+        ? DynamicLibrary.open(DYNAMIC_LIBRARY_FILE_NAME_ANDROID)
+        : Platform.isMacOS
+            ? DynamicLibrary.open(DYNAMIC_LIBRARY_FILE_NAME_MACOS)
+            : DynamicLibrary.process();
+  }
+
+  /// Computes a greeting for the given name using the native function
+  static String randomPhrase(int digits) {
+    if (_bindings == null) return "ERROR: The library is not initialized 🙁";
+
+    // Native call
+    final ptrResult = _bindings.random_phrase(digits);
+
+    // Cast the result pointer to a Dart string
+    final result = ptrResult.cast<Utf8>().toDartString();
+
+    // Clone the given result, so that the original string can be freed
+    final resultCopy = "" + result;
+
+    // Free the native value
+    InfraDID._free(result);
+
+    return resultCopy;
+  }
+
+  static String substrateAddress(String seed, int prefix) {
+    if (_bindings == null) return "ERROR: The library is not initialized 🙁";
+
+    final seedPtr = seed.toNativeUtf8().cast<Utf8>();
+
+    // Native call
+    final ptrResult = _bindings.substrate_address(seedPtr, prefix);
+
+    // Cast the result pointer to a Dart string
+    final result = ptrResult.cast<Utf8>().toDartString();
+
+    // Clone the given result, so that the original string can be freed
+    final resultCopy = "" + result;
+
+    // Free the native value
+    InfraDID._free(result);
+
+    return result;
+  }
+
+  static String generateSS58DID(String networkId) {
+    if (_bindings == null) return "ERROR: The library is not initialized 🙁";
+
+    final networkIdPtr = networkId.toNativeUtf8().cast<Utf8>();
+
+    // Native call
+    final ptrResult = _bindings.generate_ss58_did(networkIdPtr);
+
+    // Cast the result pointer to a Dart string
+    final result = ptrResult.cast<Utf8>().toDartString();
+
+    // Clone the given result, so that the original string can be freed
+    final resultCopy = "" + result;
+
+    // Free the native value
+    InfraDID._free(result);
+
+    return result;
+  }
+
+  static String didToHexPublicKey(String did) {
+    if (_bindings == null) return "ERROR: The library is not initialized 🙁";
+
+    final didPtr = did.toNativeUtf8().cast<Utf8>();
+
+    // Native call
+    final ptrResult = _bindings.did_to_hex_public_key(didPtr);
+
+    // Cast the result pointer to a Dart string
+    final result = ptrResult.cast<Utf8>().toDartString();
+
+    // Clone the given result, so that the original string can be freed
+    final resultCopy = "" + result;
+
+    // Free the native value
+    InfraDID._free(result);
+
+    return result;
+  }
+
+  static String ss58AdressToDID(String address, String networkId) {
+    if (_bindings == null) return "ERROR: The library is not initialized 🙁";
+
+    final addressPtr = address.toNativeUtf8().cast<Utf8>();
+    final networkIdPtr = networkId.toNativeUtf8().cast<Utf8>();
+
+    // Native call
+    final ptrResult = _bindings.ss58_address_to_did(addressPtr, networkIdPtr);
+
+    // Cast the result pointer to a Dart string
+    final result = ptrResult.cast<Utf8>().toDartString();
+
+    // Clone the given result, so that the original string can be freed
+    final resultCopy = "" + result;
+
+    // Free the native value
+    InfraDID._free(result);
+
+    return result;
+  }
+
+  /// Releases the memory allocated to handle the given (result) value
+  static void _free(String value) {
+    final ptr = value.toNativeUtf8().cast<Utf8>();
+    return _bindings.rust_cstr_free(ptr);
+  }
+}
